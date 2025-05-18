d1404f1a4aed1747881acb1105ea55b730f53f53
tdh <tdhster@gmail.com>
2025-05-18 09:44:02 +0800

    rename _Config for safety

--- config.py
+++ config.py
@@ -2,9 +2,25 @@
 
 from dotenv import dotenv_values
 
-class Config:
-    _config = dotenv_values(".env")
 
-    LOG_LEVEL = _config.get("LOG_LEVEL")
-    if not LOG_LEVEL:
-        raise ValueError("LOG_LEVEL not found in .env")
+class _Config:
+    def __init__(self, path=".env"):
+        self._values = dotenv_values(path)
+
+    def __getattr__(self, item):
+        if item in self._values:
+            return self._values[item]
+        raise AttributeError(f"Not found '{item}' in config file.")
+
+    def get(self, item, default=None):
+        return self._values.get(item, default)
+
+    def __repr__(self):
+        return f"<Config {self._values}>"
+    
+
+config = _Config()
+
+#Example usage:
+# from config import config
+# print(config.LOG_LEVEL)  # beware intellisence hint not work, because names are dynamic
--- logger.py
+++ logger.py
@@ -1,20 +1,23 @@
 import os
 import logging
-from config import Config
+from config import config
 
 current_dir_name = os.path.basename(os.path.abspath(os.path.dirname(__file__)))
 logger = logging.getLogger(current_dir_name)
 
-logger = logging.getLogger("briefai")
-logger.setLevel(Config.LOG_LEVEL)
+logger.setLevel(config.LOG_LEVEL)
 
 if not logger.handlers:
     handler = logging.StreamHandler()
     formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s",
+        "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
     )
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
 logger.propagate = False
+
+# Example usage:
+#  from logger import logger
+logger.debug('Logger started.')
